c121f5d61bc5eb3e9954c22d6d6eb7a669ec1c15
Z <1966@QQ.com>
2020-09-30 08:42:53 +0800

    new dfs

--- DFS.cpp
+++ DFS.cpp
@@ -1,304 +1,190 @@
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-typedef long UINT64; 
-typedef struct
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+using namespace std;
+
+// 八数码状态
+typedef struct _Status{
+    int status[3][3];
+    _Status *parent;
+    _Status *next;
+}Status;
+
+// 八数码搜索
+class EightPuzzle{
+private:
+    unsigned char allHash[362880];
+    Status root;
+    Status goal;
+private:
+    int nextNumber;
+    Status next[4];
+public:
+    EightPuzzle(Status *root, Status *goal){
+        memcpy(&this->root.status, &root->status, sizeof(int)*9);
+        this->root.parent = NULL;
+        this->root.next = NULL;
+        memcpy(&this->goal.status, &goal->status, sizeof(int)*9);
+        this->goal.parent = NULL;
+        this->goal.next = NULL;
+    }
+private:
+    // 判断是否是目标状态
+    inline int IsGoal(Status *tmp){
+        return memcmp(&tmp->status, &goal.status, sizeof(int)*9);
+    }
+    // 下一个可行的状态
+    int NextStatus(Status *tmp){
+        nextNumber = 0;
+        int posi, posj;
+        for(int i = 0; i < 9; i++){
+            posi = i/3, posj = i - i/3*3;
+            if(tmp->status[posi][posj] == 0){
+                break;
+            }
+        }
+        if(posi-1 >= 0){
+            Status left = *tmp;
+            left.status[posi][posj] = left.status[posi-1][posj];
+            left.status[posi-1][posj] = 0;
+            if(allHash[Cantor(left.status)] == 0){
+                next[nextNumber] = left;
+                next[nextNumber].parent = tmp;
+                nextNumber++;
+            }
+        }
+        if(posi+1 <= 2){
+            Status right = *tmp;
+            right.status[posi][posj] = right.status[posi+1][posj];
+            right.status[posi+1][posj] = 0;
+            if(allHash[Cantor(right.status)] == 0){
+                next[nextNumber] = right;
+                next[nextNumber].parent = tmp;
+                nextNumber++;
+            }
+        }
+        if(posj-1 >= 0){
+            Status up = *tmp;
+            up.status[posi][posj] = up.status[posi][posj-1];
+            up.status[posi][posj-1] = 0;
+            if(allHash[Cantor(up.status)] == 0){
+                next[nextNumber] = up;
+                next[nextNumber].parent = tmp;
+                nextNumber++;
+            }
+        }
+        if(posj+1 <= 2){
+            Status down = *tmp;
+            down.status[posi][posj] = down.status[posi][posj+1];
+            down.status[posi][posj+1] = 0;
+            if(allHash[Cantor(down.status)] == 0){
+                next[nextNumber] = down;
+                next[nextNumber].parent = tmp;
+                nextNumber++;
+            }
+        }
+        return nextNumber;
+    }
+    // 康托展开
+    int Cantor(int arr[][3]){
+        int fac[10] = {1,1,2,6,24,120,720,5040,40320,362880};
+        int index = 0;
+        for(int i = 7; i >= 0; i--){
+            int irow = i/3, icol = i - i/3*3;
+            int count = 0;
+            for(int j = 8; j > i; j--){
+                int jrow = j/3, jcol = j - j/3*3;
+                if(arr[jrow][jcol] < arr[irow][icol]){
+                    count++;
+                }
+            }
+            index += (count*fac[8-i]);
+        }
+        return index;
+    }
+public:
+    // 深度优先搜索
+    int DFS(){
+        int depth = 0;
+        int step = 0;
+        stack<Status> openTable;
+        Status *closeTable = new Status;;
+        Status *current = closeTable;
+        Status *last;
+        Status *tmp;
+        openTable.push(root);
+        while(!openTable.empty()){
+            tmp = new Status;
+            *tmp = openTable.top();
+            openTable.pop();
+            step++;
+            current->next = tmp;
+            current = current->next;
+            if(IsGoal(tmp) == 0){
+                PrintPath(tmp);
+                freeCloseTable(closeTable);
+                return step;
+            }
+            memset(allHash, 0, 362880);
+            last = tmp;
+            depth = 0;
+            while(last != NULL){
+                allHash[Cantor(last->status)] = 1;
+                last = last->parent;
+                depth++;
+            }
+            if(depth > 8){
+                continue;
+            }
+            int nextNumber = NextStatus(tmp);
+            if(nextNumber == 0){
+                continue;
+            }
+            for(int i = 0; i < nextNumber; i++){
+                openTable.push(next[i]);
+            }
+        }
+        cout << "DFS failed." << endl;
+        freeCloseTable(closeTable);
+        return -1;
+    }
+private:
+    // 打印路径
+    void PrintPath(Status *head){
+        if(head == NULL){
+            return;
+        }
+        else{
+            PrintPath(head->parent);
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    cout << head->status[i][j];
+                }
+                cout << endl;
+            }
+            cout <<endl;
+        }
+    }
+    // 释放close表
+    void freeCloseTable(Status *closeTable){
+        Status *current;
+        while(closeTable != NULL){
+            current = closeTable->next;
+            free(closeTable);
+            closeTable = current;
+        }
+    }
+};
+
+int main()
 {
-	char x; //位置 x 和位置 y 上的数字换位
-	char y; //其中 x 是 0 所在的位置
-} EP_MOVE;
-
-#define SIZE 3 //8数码问题，理论上本程序也可解决15数码问题，
-#define NUM SIZE * SIZE //但 move_gen需要做很多修改，输入初始和结束状态的部分和check_input也要修改
-#define MAX_NODE 1000000
-#define MAX_DEP 100
-#define XCHG(a, b) { a=a + b; b=a - b; a=a - b; } 
-#define TRANS(a, b)
-/*{ long iii; (b)=0; for(iii=0; iii < NUM; iii++) (b)=((b) << 4) + a[iii]; }*/ 
-//将数组 a 转换为一个64 位的整数 b 
-#define RTRANS(a,b){long iii; UINT64 ttt=(a); for(iii=NUM-1;iii>=0;iii--){b[iii]=ttt & 0xf;ttt>>=4;}}
-
-//将一个 64 位整数 a 转换为数组 b
-
-typedef struct EP_NODE_Tag
-{
-	UINT64 v; //保存状态，每个数字占4个二进制位,可解决16数码问题
-	struct EP_NODE_Tag *prev; //	父节点
-	struct EP_NODE_Tag *small, *big;
-}EP_NODE;
-EP_NODE m_ar[MAX_NODE];
-EP_NODE *m_root;
-long m_depth; //搜索深度
-EP_NODE m_out[MAX_DEP]; //输出路径
-
-long move_gen(EP_NODE *node, EP_MOVE *move)
-{
-	long pz; //0	的位置
-	UINT64 t=0xf;
-	for(pz=NUM - 1; pz >= 0; pz--)
-	{
-		if((node->v & t) == 0)
-		{ break; //	找到 0 的位置
-		}
-	t<<=4;
-	}
-	switch(pz)
-	{
-		case 0: move[0].x=0;
-				move[0].y=1;
-				move[1].x=0;
-				move[1].y=3; 
-				return 2;
-		case 1: move[0].x=1;
-				move[0].y=0;
-				move[1].x=1;
-				move[1].y=2;
-				move[2].x=1;
-				move[2].y=4; 
-				return 3;
-		case 2: move[0].x=2;
-				move[0].y=1;
-				move[1].x=2;
-				move[1].y=5;
-		case 3: move[0].x=3;
-				move[0].y=0;
-				move[1].x=3;
-				move[1].y=6;
-				move[2].x=3;
-				move[2].y=4; 
-				return 3;
-		case 4: move[0].x=4;
-				move[0].y=1;
-				move[1].x=4;
-				move[1].y=3;
-				move[2].x=4;
-				move[2].y=5;
-				move[3].x=4;
-				move[3].y=7; 
-				return 4;
-		case 5: move[0].x=5;
-				move[0].y=2;
-				move[1].x=5;
-				move[2].x=5;
-				move[2].y=8; return 3;
-		case 6: move[0].x=6;
-				move[0].y=3;
-				move[1].x=6;
-				move[1].y=7; 
-				return 2;
-		case 7: move[0].x=7;
-				move[0].y=6;
-				move[1].x=7;
-				move[1].y=4;
-				move[2].x=7;
-				move[2].y=8; 
-				return 3;
-		case 8: move[0].x=8;
-				move[0].y=5;
-				move[1].x=8;
-				move[1].y=7;
-	}
-	return 0;
-}
-
-long mov(EP_NODE *n1, EP_MOVE *mv, EP_NODE *n2)
-// 走一步，返回走一步后的结果
-{
-	char ss[NUM];
-	RTRANS(n1->v, ss);
-	XCHG(ss[mv->x], ss[mv->y]); 
-	TRANS(ss, n2->v);
-	return 0;
-}
-
-long add_node(EP_NODE *node, long r)
-{
-	EP_NODE *p=m_root; 
-	EP_NODE *q; 
-	while(p)
-	{ 
-		q=p;
-		if(p->v == node->v) 
-			return 0; 
-		else if(node->v > p->v) 
-			p=p->big;
-		else if(node->v < p->v) 
-			p=p->small;
-	}
-	m_ar[r].v=node->v; 
-	m_ar[r].prev=node->prev; 
-	m_ar[r].small=NULL; 
-	m_ar[r].big=NULL; 
-	if(node->v > q->v)
-		q->big= &m_ar[r];
-	else if(node->v < q->v)
-		q->small= &m_ar[r];
-	return 1;
-
-}
-
-/* 得到节点所在深度 */
-long get_node_depth(EP_NODE *node)
-{ 
-	long d=0; 
-	while(node->prev)
-	{ 
-		d++;
-		node=node->prev;
-	}
-	return d;
-}
-
-/* 返回值：成功－返回搜索节点数，节点数不够－	(-1) ，无解－ (-2)*/ 
-long bfs_search(char *begin, char *end)
-{
-	long h=0,r=1,c,i,j;
-	EP_NODE l_end, node, *pnode;
-	EP_MOVE mv[4]; // 每个局面最多4种走法
-	TRANS(begin, m_ar[0].v); 
-	TRANS(end, l_end.v); 
-	m_ar[0].prev=NULL; 
-	m_root=m_ar;
-	m_root->small=NULL; 
-	m_root->big=NULL;
-	while((h < r) && (r < MAX_NODE - 4))
-	{ 
-		c=move_gen(&m_ar[h], mv); 
-		for(i=0; i < c; i++)
-		{ 
-			mov(&m_ar[h], &mv[i], &node); 
-			node.prev= &m_ar[h];
-			if(node.v == l_end.v)
-			{ 
-				pnode= &node; j=0;
-				while(pnode->prev)
-				{
-					m_out[j]=*pnode; j++;
-					pnode=pnode->prev;
-				}
-				m_depth=j;
-				return r;
-			}
-			if(add_node(&node, r)) 
-				r++; //	只能对历史节点中没有的新节点搜索，否则会出现环
-		} 
-		h++;
-		printf("\rSearch...%9d/%d @ %d", h, r, get_node_depth(&m_ar[h]));
-	}
-	if(h == r)
-		return -2;
-	else
-		return -1; 
-
-}
-
-long check_input(char *s, char a, long r)
-{
-	long i;
-	for(i=0; i < r; i++)
-	{ 
-		if(s[i] == a - 0x30) return 0; } 
-	return 1;
-}
-
-long check_possible(char *begin, char *end)
-{ 
-	char fs;
-	long f1=0, f2=0; 
-	long i, j;
-	for(i=0; i < NUM; i++)
-	{	
-		fs=0;
-		for(j=0; j < i; j++)
-		{
-			if((begin[i] != 0) && (begin[j] != 0) && (begin[j] < begin[i])) 
-				fs++;
-		}
-		f1+=fs; fs=0;
-		for(j=0; j < i; j++)
-		{ 
-			if((end[i] != 0) && (end[j] != 0) && (end[j] < end[i]))
-				fs++;
-		}
-		f2+=fs;
-		if((f1 && 1) == (f2 && 1)) return 1; 
-		else return 0;
-	}
-}
-
-void output(void)
-{ 
-	long i, j, k; 
-	char ss[NUM];
-	for(i=m_depth - 1; i >= 0; i--)
-	{
-		RTRANS(m_out[i].v, ss); 
-		for(j=0; j < SIZE; j++)
-		{ 
-			for(k=0; k < SIZE; k++)
-				printf("%2d",ss[SIZE * j+k]);
-		printf("\n");
-		}
-		printf("\n");
-	}
-}
-
-
-int main(void)
-{ 
-	char s1[NUM]; 
-	char s2[NUM]; 
-	long r;
-	char a;
-	printf("请输入开始状态 :"); 
-	r=0;
-	while(r < NUM)
-	{ 
-		a=getchar();
-		if(a >= 0x30 && a < 0x39 && check_input(s1, a, r))
-		{ 
-			s1[r++]=a - 0x30;
-		}
-	}
-	printf("\n请输入结束状态 :"); 
-	r=0;
-	while(r<NUM)
-	{ 
-		a=getchar();
-		if(a >= 0x30 && a < 0x39 && check_input(s2, a, r))
-		{ 
-			s2[r++]=a - 0x30;
-		}
-	}
-
-	printf("\n"); 
-	if(check_possible(s1, s2))
-	{ 
-		r=bfs_search(s1, s2); 
-		printf("\n");
-		if(r >= 0)
-
-		{ 
-			printf("查找深度=%d,所有的方式 =%ld\n", m_depth, r); 
-			output();
-		}
-		else if(r == -1)
-		{ 
-			printf("没有找到路径 .\n");
-		}
-
-		else if(r == -2)
-		{
-			printf("这种状态变换没有路径到达\n");
-		}
-		else
-		{
-			printf("不确定的错误 .\n");
-		}
-	}
-	else
-	{ 
-		printf("不允许这样移动!\n");
-	}
-	return 0;
+    Status init = {2,8,3,1,0,4,7,6,5,0,NULL};
+    Status goal = {8,1,3,2,0,4,7,6,5,0,NULL};
+    EightPuzzle ep = EightPuzzle(&init, &goal);
+    cout << "step: " << ep.DFS() << endl;
+    return 0;
 }
